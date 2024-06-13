0e9871e3132b84c7ba98077c172a98a2b9bd4795
TrungBaizen <trungbaizen@gmail.com>
2024-06-13 20:08:41 +0700

    nopbai

---
+++ src/main/java/com/example/thuchanhqlkh/service/ICustomerService.java
@@ -0,0 +1,16 @@
+package com.example.thuchanhqlkh.service;
+import com.example.thuchanhqlkh.model.Customer;
+
+import java.util.List;
+
+public interface ICustomerService {
+    List<Customer> findAll();
+
+    void save(Customer customer);
+
+    Customer findById(int id);
+
+    void update(int id, Customer customer);
+
+    void remove(int id);
+}
